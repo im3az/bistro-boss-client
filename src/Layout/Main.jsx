9cd2c930be9c428c4f5477fb40596fc93c4cedf2
Imtiaz <im3azchowdhury@gmail.com>
2023-11-10 10:45:26 +0600

    initial structure created

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/shared/footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
