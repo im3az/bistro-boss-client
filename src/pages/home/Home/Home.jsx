1b5857733b21c220eb2170053290df301fecdd12
Imtiaz <im3azchowdhury@gmail.com>
2023-11-10 15:11:22 +0600

    footer added and navbar edited

--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
-    return (
-      <div>
-        <h2>This is home.</h2>
-      </div>
-    );
-  };
-  
-  export default Home;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
